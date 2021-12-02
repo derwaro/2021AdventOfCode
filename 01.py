0434f4a179afa78dda91b4b9530a257ea8dc7c3b
derwaro <chrdina@gmail.com>
2021-12-02 15:57:34 +0100

    day 01 finished

--- 01.py
+++ 01.py
@@ -1,2 +1,25 @@
 #!/usr/bin/python3
 
+f = open('01_input.txt')
+
+inputlist = [[i, int(e)] for i, e in enumerate(f)]
+
+increases = 0
+decreases = 0
+nextinput = 0
+
+for x in inputlist:    
+    if x[0] != 0:
+        if inputlist[nextinput][1] < x[1]:
+            print(str(x[1]) + " increased")
+            increases += 1
+            nextinput += 1
+        else:
+            print(str(x[1]) + " decreased")
+            decreases += 1
+            nextinput += 1
+    else:
+        print(str(x[1]) + " N/A")
+
+print("increases: " + str(increases))
+print("decreases: " + str(decreases))
