c45379acd9ad03a26c7948c0c00f5632230b78c7
derwaro <chrdina@gmail.com>
2021-12-21 15:55:32 +0100

    02 try

--- 02.py
+++ 02.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
+#part 1
 f = open('02_input.txt')
-coordinates = [0,0]
+coordinates = [0,0] #forward, depth
 path = []
 count = 0
 
@@ -12,15 +13,32 @@ for i in f.readlines():
 
 
 for a in path:
-    count += 1
-    print(count)
-    print(a[0], a[-1])
     if a[0] == "f":
         coordinates[0] += int(a[-1])
     elif a[0] == "u":
         coordinates[1] -= int(a[-1])
     elif a[0] == "d":
         coordinates[1] += int(a[-1])
+
+
+print(coordinates[0]*coordinates[1])
+
+
+#part 2
+
+coordinates = [0,0,0] #forward, depth, aim
+print(coordinates)
+
+for a in path:
+    if a[0] == "f":
+        coordinates[0] += int(a[-1])
+        coordinates[1] += coordinates[1]*coordinates[2]
+    elif a[0] == "u":
+        #coordinates[1] -= int(a[-1])
+        coordinates[2] -= int(a[-1])
+    elif a[0] == "d":
+        #coordinates[1] += int(a[-1])
+        coordinates[2] += int(a[-1])
 
 print(coordinates)
 print(coordinates[0]*coordinates[1])
