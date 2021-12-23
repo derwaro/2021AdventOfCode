43dc8708318f4fdeedf7efbf544cc38fc50688ea
derwaro <chrdina@gmail.com>
2021-12-23 14:02:32 +0100

    day 03 part 1 finished

--- 03.py
+++ 03.py
@@ -3,16 +3,21 @@
 
 f = open('03_input.txt')
 
-input = [line[:-1] for line in f.readlines()]
+input_bin = [line[:-1] for line in f.readlines()]
 input_formatted = [[],[],[],[],[],[],[],[],[],[],[],[]]
 counter = 0
+gamma = ""
+epsilon = ""
 
-for i in input:
-    for a in i:
-        if counter < 7:
-            input_formatted[counter] = a
-            counter += 1
-        else:
-            counter = 0
+#get first position from line in input into a list
+for i in range(0,12):
+    for a in input_bin:
+        input_formatted[i].append(a[i])
 
+for i in input_formatted:
+    a = max(set(i), key = i.count) #get maximum occurence of number
+    b = min(set(i), key = i.count) #get minimum occurence of number
+    gamma += a
+    epsilon += b
 
+print(int(gamma, 2)*int(epsilon, 2))
